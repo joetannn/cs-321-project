39bfb2a51d7428a4e542493e0a8ed1334587ad4a
Bradley Dufour <dufour.bradley@gmail.com>
2019-10-26 23:39:29 -0400

    Add skeleton for SkillsComparer

---
+++ SkillsComparer.py
@@ -0,0 +1,15 @@
+class SkillsComparer:
+
+    #List of strings representing tech words, helps filter out unecessary terms for getExtraJobSkills()
+    skillDictionary = []
+
+    def __init__(self, dictionary):
+        self.skillDictionary = dictionary
+
+    #Both arguments are expected to be lists of Strings
+    def getExtraJobSkills(skillsList, jobSkills):
+        pass
+
+    #Both arguments are expected to be lists of Strings
+    def getExtraSkillsListSkills(skillsList, jobSkills):
+        pass
