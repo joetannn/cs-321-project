6d472081ad3d1b86248f365c8e92aab3daa3c32e
Bradley Dufour <dufour.bradley@gmail.com>
2019-10-27 00:00:59 -0400

    Fill in methods

--- SkillsComparer.py
+++ SkillsComparer.py
@@ -9,9 +9,19 @@ class SkillsComparer:
     #Both arguments are expected to be lists of Strings
     # @return list of strings representing extra skills
     def getExtraJobSkills(self, skillsList, jobSkills):
-        pass
+        extra = [skill for skill in jobSkills if (skill in self.skillDictionary and not (skill in skillsList))]
+        return extra
 
     #Both arguments are expected to be lists of Strings
     # @return list of strings representing extra skills
     def getExtraSkillsListSkills(self, skillsList, jobSkills):
-        pass
+        extra = [skill for skill in skillsList if not skill in jobSkills]
+        return extra
+
+comp = SkillsComparer([10, 20, 30, 40, 50])
+skillsList = [10, 15, 18, 21, 30, 25, 40]
+jobSkills = [10, 20, 30, 40, 50, 55, 25, 18, 17]
+extra = comp.getExtraJobSkills(skillsList, jobSkills)
+print(extra)
+extra = comp.getExtraSkillsListSkills(skillsList, jobSkills)
+print(extra)
