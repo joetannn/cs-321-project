6782ae90c40aee88cf8d01382928eb6d12ac4140
_Epyon <TheEpyonProject@gmail.com>
2019-11-15 15:18:35 -0500

    added a method to check the link

--- CS321CoverLetterGenerator/src/app/user-form/user-form.component.ts
+++ CS321CoverLetterGenerator/src/app/user-form/user-form.component.ts
@@ -47,7 +47,21 @@ export class UserFormComponent implements OnInit {
     //if this fails, there is no connectivity
     this.showMath();
     //
-    this.sendUserData();
+    if (this.validURL()) {
+      this.sendUserData();
+    } else {
+      console.log("Incorrect link");
+    }
+  }
+
+  validURL() {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(this.user.link);
   }
 
   addSkill() {
